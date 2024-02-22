f257e1e4c752d1286228047f5a4914ff631c5e34
battasrikanth <battasrikanth21@gmail.com>
2024-02-22 22:10:15 +0530

    first commit

---
+++ portfolio.js
@@ -0,0 +1,8 @@
+function showBlock() {
+  var x = document.getElementById("feedback");
+  if (x.style.display === "none") {
+    x.style.display = "block";
+  } else {
+    x.style.display = "none";
+  }
+}
